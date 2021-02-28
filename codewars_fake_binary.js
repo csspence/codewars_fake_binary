3382ecb391e8dff30d0496a27904a9be34157aea
Crew Spence <csspence123@gmail.com>
2021-02-28 06:18:04 -0600

    Wrote working solution.

--- codewars_fake_binary.js
+++ codewars_fake_binary.js
@@ -2,6 +2,11 @@
 Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 */
 
-function fakeBin(x){
+const fakeBin = (x) => {
+  let str = '';
+  for(let i = 0; i < x.length; i++) {
+    parseInt(x[i]) < 5 ? str += '0' : str += '1';
+  }
 
+  return str;
 }
